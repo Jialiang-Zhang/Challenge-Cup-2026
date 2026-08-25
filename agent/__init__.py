fcfbe9d878926feafde73c9064fddd9875ee4561
Jialiang-Zhang <obito2026210@gmail.com>
2026-08-26 00:54:04 +0800

    chore: mark HORA-Math implementation release candidate

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,4 +1,6 @@
 from .models import AgentConfig
 from .orchestrator import HORAEngine
 
-__all__ = ["AgentConfig", "HORAEngine"]
+__version__ = "1.0.0-rc1"
+
+__all__ = ["AgentConfig", "HORAEngine", "__version__"]
